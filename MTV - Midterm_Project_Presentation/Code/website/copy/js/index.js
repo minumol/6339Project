5bea11fbaff8f7d57666dd29a82fc40ea177981b
minumol <minumol.joseph@mavs.uta.edu>
2015-03-26 22:35:26 -0600

    Motor Tweet Vogue - Mid term Project Presentation
    Documents, Dataset, Code

---
+++ MTV - Midterm_Project_Presentation/Code/website/copy/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	change_navbar(0);
+	$('.selectpicker').selectpicker({
+      style: 'btn-info',
+      size: 4
+  });	
+});
+
---
+++ MTV - Midterm_Project_Presentation/Code/website/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<link rel="icon" type="image/png" href="image/mtvLogo.png"/>
+		<title>Motor Tweet Vogue</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="bootstrap-3.3.2-dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="iCheck/skins/square/blue.css" rel="stylesheet">
+		<link href="css/index.css" rel="stylesheet">
+	</head>
+
+	<body>
+																																																																																		
+	
+	<div class="header">
+		<div class="container">        						
+			<div class="row">
+				<div class="col-md-5"> </div>					
+				<div class="col-md-2">
+					<a href="http://idir.uta.edu/"><img class="img-responsive" id="mtv_logo" src="image/mtvLogo.png" alt="IDIR Logo"></a>
+				</div>
+				<div class="col-md-5"> </div>
+			</div>								
+		</div>
+	</div>
+	<nav class="navbar navbar-inverse">
+		<div class="container span12 pagination-centered">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">Motor Tweet Vogue</a>
+			</div>
+		</div>
+	</nav>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>Explore the trends in Car Brands</h1>
+        <p>Tells you the current trend and visualise it for you.</p>
+        <p><a href="#" class="btn btn-primary btn-large">more on MTV &raquo;</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span6">
+		<table>
+          <h2>Changing trends in States</h2>
+				<tr><td>
+				<select class="states" >
+						<option>Select State</option>
+						<option>Alabama </option>
+						<option>Alaska </option>
+						<option>Arizona </option>
+				</select>
+				</td>
+				<td>
+				<select class="states" >
+						<option>Select Year</option>
+						<option>1949 </option>
+						<option>1950 </option>
+						<option>1951 </option>
+				</select>
+				</td></tr>
+				<img src="image/states.png"/>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+		</table>
+        </div>
+        <div class="span6">
+          <h2>Changing Trends in Brands</h2>
+		  <img src="image/states.png"/>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+	  </div>
+	  <div class="row">
+		<div class="span6">
+          <h2>Current trends in States</h2>
+		  <img src="image/states.png"/>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+		<div class="span6">
+          <h2>Current trends in brands</h2>
+		  <img src="image/states.png"/>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <footer>
+        <p>&copy; MTV 2015</p>
+      </footer>
+    </div> <!-- /container -->	  
+
+ 
+		
+
+
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="bootstrap-3.3.2-dist/js/bootstrap.min.js"></script>
+		<script src="js/index.js"></script>
+		<script src="iCheck/icheck.js"></script>
+		
+	</body>
+</html>
+
